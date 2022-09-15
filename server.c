d55e3e2b3ad23fd21d81a90fc131882fb1579d5f
Dongli Si <sidongli1997@gmail.com>
2022-09-15 23:52:36 +0800

    server.c

---
+++ server.c
@@ -0,0 +1,46 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h>
+
+int main(int argc, char *argv[])
+{
+	int listenfd = 0, connfd = 0, fd, n;
+	struct sockaddr_in serv_addr;
+	char buf[BUFSIZ];
+
+	char sendBuff[1025];
+	time_t ticks;
+
+	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+	memset(&serv_addr, '0', sizeof(serv_addr));
+	memset(sendBuff, '0', sizeof(sendBuff));
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_port = htons(5000);
+
+	int option = 1;
+	setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(option));
+
+	bind(listenfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+
+	listen(listenfd, 10);
+
+	while (1) {
+		connfd = accept(listenfd, (struct sockaddr *) NULL, NULL);
+		fd = open("/home/d/linux/build/kernel/events/core.o", O_RDONLY);
+		while ((n = read(fd, buf, BUFSIZ)) > 0)
+			if (write(connfd, buf, n) != n)
+				printf("write error\n");
+		close(fd);
+		close(connfd);
+	}
+}
