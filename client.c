3853686b608e5b3ca8aace64ba84d52fff6f357b
Dongli Si <sidongli1997@gmail.com>
2022-09-16 07:18:25 +0800

    client.c

--- client.c
+++ client.c
@@ -12,15 +12,22 @@
 #include <errno.h>
 #include <arpa/inet.h>
 
-int main(int argc, char *argv[])
+void get_opath(int argc, char **argv, char **opath)
 {
-	int sockfd = 0, n = 0, fd, iovs_len[argc];
-	char recvBuff[BUFSIZ];
-	struct sockaddr_in serv_addr;
-	struct stat statbuf;
-	struct iovec *iovs;
+	int i;
 
+	for (i = 0; i < argc; i++) {
+		if (!strcmp("-o", argv[i])) {
+			*opath = argv[i + 1];
+			return;
+		}
+	}
+}
+
+void gcc(char argc, char **argv)
+{
 	char *args[argc + 1];
+
 	for (int i = 1; i < argc; i++) {
 		args[i] = malloc(strlen(argv[i]) + 1);
 		strcpy(args[i], argv[i]);
@@ -28,33 +35,18 @@ int main(int argc, char *argv[])
 	args[0] = "/usr/lib64/ccache/gcc";
 	args[argc] = NULL;
 
-	// execvp(args[0], args);
+	execvp(args[0], args);
+}
 
+int get_sockfd()
+{
+	int sockfd = 0;
+	struct sockaddr_in serv_addr;
 
-	iovs = calloc(argc, sizeof(struct iovec));
-
-	for (int i = 0; i < argc; i++) {
-		iovs[i].iov_base = args[i];
-		iovs[i].iov_len = strlen(args[i]) + 1;
-		iovs_len[i] = iovs[i].iov_len;
-	}
-
-/*
-	fd = open("/home/d/linux/Makefile", O_RDONLY);
-	fstat(fd, &statbuf);
-	close(fd);
-
-	printf(" %9jd", statbuf.st_size);
-
-	return 0;
-*/
-
-	memset(recvBuff, '0', sizeof(recvBuff));
 	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
 		printf("\n Error : Could not create socket \n");
 		return 1;
 	}
-
 
 	memset(&serv_addr, '0', sizeof(serv_addr));
 
@@ -66,24 +58,56 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	if (connect
-	    (sockfd, (struct sockaddr *) &serv_addr,
-	     sizeof(serv_addr)) < 0) {
+	if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
 		printf("\n Error : Connect Failed \n");
 		return 1;
 	}
 
-	n = write(sockfd, &argc, 4);
-	write(sockfd, iovs_len, sizeof(int) * argc);
+	return sockfd;
+}
+
+void distcc(int argc, char **argv)
+{
+	int sockfd, i, fd, n, iovs_len[argc];
+	char buf[BUFSIZ], *opath;
+	struct iovec *iovs;
+
+	sockfd = get_sockfd();
+	iovs = calloc(argc, sizeof(struct iovec));
+	iovs[0].iov_base = "/usr/lib64/ccache/gcc";
+	iovs[0].iov_len = strlen(iovs[0].iov_base) + 1;
+	iovs_len[0] = iovs[0].iov_len;
+	for (i = 1; i < argc; i++) {
+		iovs[i].iov_base = argv[i];
+		iovs[i].iov_len = strlen(argv[i]) + 1;
+		iovs_len[i] = iovs[i].iov_len;
+	}
+
+	write(sockfd, &argc, sizeof(int));
+	write(sockfd, iovs_len, sizeof(iovs_len));
 	writev(sockfd, iovs, argc);
 
-/*
-	fd = open("/home/d/test/process.o", O_CREAT | O_WRONLY, 0644);
-	while ((n = read(sockfd, recvBuff, BUFSIZ)) > 0) {
-		if (write(fd, recvBuff, n) != n)
+	get_opath(argc, argv, &opath);
+
+	fd = open(opath, O_CREAT | O_WRONLY, 0644);
+	while ((n = read(sockfd, buf, BUFSIZ)) > 0) {
+		if (write(fd, buf, n) != n)
 			printf("write error %d\n", n);
 	}
 	close(fd);
-*/
+}
+
+int main(int argc, char *argv[])
+{
+	int fd;
+	struct stat statbuf;
+
+	fd = open("/home/d/linux/Makefile", O_RDONLY);
+	fstat(fd, &statbuf);
+	close(fd);
+
+	// printf("%9jd\n", statbuf.st_size);
+
+	distcc(argc, argv);
 	return 0;
 }
